1bddd245206452a677f1000a48f2d2724ec56ef6
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-15 15:25:26 +0800

    Resend (sending emails)

--- auth.ts
+++ auth.ts
@@ -37,6 +37,18 @@ export const {
 		},
 	},
 	callbacks: {
+		async signIn({ user, account }) {
+			// Allow 0Auth without email verification
+			if (account?.provider !== "credentials") return true;
+
+			const existingUser = await getUserById(user.id);
+
+			// Prevent sign in without email verification
+			if (!existingUser?.emailVerified) return false;
+
+			// TODO: Add 2FA check
+			return true;
+		},
 		async session({ token, session }) {
 			console.log({ sessionToken: token });
 
