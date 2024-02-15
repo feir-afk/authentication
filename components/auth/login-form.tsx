89db80c60870451d2355533e6d6f152c1a015dbf
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-15 14:24:35 +0800

    OAuth (Google & Github)

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -13,8 +13,12 @@ import { FormError } from '../form-error'
 import { FormSuccess } from '../form-success'
 import { login } from '@/actions/login'
 import { startTransition, useState, useTransition } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams()
+    const urlError = searchParams.get("error") === ")AuthAccountNotLinked"
+        ? "Email already in use with different provider!" : ""
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition()
@@ -32,8 +36,9 @@ export const LoginForm = () => {
 
         startTransition(() => {
             login(values).then((data) => {
-                setError(data.error)
-                setSuccess(data.success)
+                setError(data?.error)
+                // TODO: Add when we add 2FA
+                // setSuccess(data?.success)
             })
         })
     }
@@ -78,7 +83,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
 
                     <Button type='submit'
